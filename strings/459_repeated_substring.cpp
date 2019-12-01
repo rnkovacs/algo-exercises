491bfec449f41fa8c29b0ae1d43efce00d40147e
Reka Kovacs <rekanikolett@gmail.com>
2019-12-01 13:21:26 +0100

    Make repeated substring more C++y

--- strings/459_repeated_substring.cpp
+++ strings/459_repeated_substring.cpp
@@ -4,45 +4,38 @@
  * Examples: Input: "abab"  Output: true
  *           Input: "aba"   Output: false
  *
- * Runtime: 20 ms, faster than 98.00% of C++ online submissions.
- * Memory usage: 12.5 MB, less than 85.71% of C++ online submissions.
+ * Runtime: 8 ms, faster than 99.79% of C++ online submissions.
+ * Memory usage: 12.8 MB, less than 85.71% of C++ online submissions.
  */
 
 #include <iostream>
 #include <vector>
 
 bool repeatedSubstringPattern(std::string s) {
-    int len = s.size();
-    if (len < 2)
-      return false;
+  int len = s.size();
+  if (len < 2)
+    return false;
 
-    char pfirst = s[0];
-    char plast = s[len-1];
-    std::vector<std::string> patterns;
+  char pfirst = s[0];
+  char plast = s[len-1];
+  std::vector<std::string> patterns;
 
-    for (int i = len/2; i > 0; --i) {
-      if (!(len % i) && s[i-1] == plast)
-        patterns.push_back(s.substr(0, i));
-    }
-    
-    for (std::string pattern : patterns) {
-      bool fail = false;
-      int plen = pattern.size();
-      int i=plen;
-      int j=0;
-      while (i < len && j < plen) {
-        if (s[i] != pattern[j]) {
-          fail = true;
-          break;
-        }
-        j = (j == plen-1) ? 0 : (j+1);
-        ++i;
-      }
-      if (!fail)
+  for (int i = len/2; i > 0; --i) {
+    if (!(len % i) && s[i-1] == plast)
+      patterns.push_back(s.substr(0, i));
+  }
+  
+  for (std::string pattern : patterns) {
+    int plen = pattern.size();
+    for (int i=plen; i<len; i+=plen) {
+      if (s.substr(i, plen) != pattern)
+        break;
+      if (i+plen == len)
         return true;
     }
-    
-    return false;
+  }
+  
+  return false;
 }
 
 int main() {
