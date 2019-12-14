f4a4c8d28cd236c45b5705147d8bfb4e2ef273c8
Reka Kovacs <rekanikolett@gmail.com>
2019-12-14 13:14:59 +0100

    Reverse bits

---
+++ leetcode/190_reverse_bits.cpp
@@ -0,0 +1,21 @@
+/* Reverse bits of a given 32-bit unsigned integer.
+ *
+ * Runtime: 0 ms, faster than 100% of C++ online submissions.
+ * Memory usage: 8.2 MB, less than 54.17% of C++ online submissions.
+ */
+
+#include <iostream>
+
+uint32_t reverseBits(uint32_t n) {
+  uint32_t res = 0;
+  for (int i=0; i<32; ++i) {
+    res |= (n & 1) << (31 - i);
+    n >>= 1;
+  }
+  return res;
+}
+
+int main() {
+  uint32_t u = 43261596;
+  std::cout << u << '\n' << reverseBits(u) << '\n';
+}
